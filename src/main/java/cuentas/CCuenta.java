000e59b56ff0d74d0c4edb671e482c3c3bd33168
KevinMayan94 <kevinmayan94@gmail.com>
2025-03-03 13:58:08 +0100

    añadiendo javadoc

--- src/main/java/cuentas/CCuenta.java
+++ src/main/java/cuentas/CCuenta.java
@@ -1,92 +1,121 @@
 package cuentas;
+/**
+ * la clase CCuenta represta una cuenta bancaria con operaciones basicas como
+ * deposito de dinero y retirada y vonsulta de efectivo, ademas contiene informacion
+ * de la cuenta y el dueño de esta
+ * @author mayan
+ */
 
 public class CCuenta {
 
-    /**
-     * @return the nombre
+    //Setters y getters de todos los parametros necesarios
+    
+    /***
+     * getter del nombre
+     * @return nombre
      */
     public String getNombre() {
         return nombre;
     }
 
-    /**
-     * @param nombre the nombre to set
+    /**setter del nombre
+     * @param nombre del titular
      */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
-    /**
-     * @return the cuenta
+    /**getter del numero de cuenta
+     * @return numero de cuenta
      */
     public String getCuenta() {
         return cuenta;
     }
 
-    /**
-     * @param cuenta the cuenta to set
+    /**setter del numero de cuenta
+     * @param cuenta numero de cuenta
      */
     public void setCuenta(String cuenta) {
         this.cuenta = cuenta;
     }
 
-    /**
-     * @return the saldo
+    /**getter del saldo
+     * @return  saldo de la cuenta
      */
     public double getSaldo() {
         return saldo;
     }
 
-    /**
-     * @param saldo the saldo to set
+    /**setter del saldo disponible
+     * @param saldo disponible
      */
     public void setSaldo(double saldo) {
         this.saldo = saldo;
     }
 
-    /**
-     * @return the tipoInterés
+    /**getter del tipo de interes
+     * @return tipoInterés
      */
     public double getTipoInterés() {
         return tipoInterés;
     }
 
-    /**
-     * @param tipoInterés the tipoInterés to set
+    /**setter tipo de interes
+     * @param tipoInterés de la cuenta
      */
     public void setTipoInterés(double tipoInterés) {
         this.tipoInterés = tipoInterés;
     }
 
 
-    private String nombre;
-    private String cuenta;
-    private double saldo;
-    private double tipoInterés;
+    private String nombre;              //Nombre del titular
+    private String cuenta;              //Numero de cuenta
+    private double saldo;               //saldo actual de la cuenta
+    private double tipoInterés;         //tipo de interes de la cuenta
 
+    /***
+     * Constructor de CCuenta. Inicia una cuenta vacia
+     */
     public CCuenta()
     {
     }
 
+    /**
+     * Constructor de Ccuenta con informacion especifica.
+     * 
+     * @param nom Nombre del titular
+     * @param cue Numero de cuenta
+     * @param sal Saldo de la cuenta
+     * @param tipo Tipo de Interes de la cuenta
+     */
     public CCuenta(String nom, String cue, double sal, double tipo)
     {
         nombre =nom;
         cuenta=cue;
         saldo=sal;
     }
-
-    public double estado()
+    //devuelve el saldo actual de la cuenta
+    public double estado() //metodo para recibir el saldo de la cuenta
     {
         return getSaldo();
     }
 
+    /**Desposita una cantidad de dinero en la cuenta
+     * lanza una excepcion si la cantidad es negativa
+     * @param cantidad a ingresar
+     * @throws java.lang.Exception excepcion si la cantidad es negativa
+     */
     public void ingresar(double cantidad) throws Exception
     {
         if (cantidad<0)
             throw new Exception("No se puede ingresar una cantidad negativa");
         setSaldo(getSaldo() + cantidad);
     }
-
+    /**retira una cantidad de dinero en la cuenta
+     * lanza una excepcion si la cantidad es negativa o si no hay fondos suficientes
+     * @param cantidad a retirar
+     * @throws java.lang.Exception excepcion si la cantidad es negativa o no hay suficiente dinero
+     */
     public void retirar(double cantidad) throws Exception
     {
         if (cantidad <= 0)
--- src/main/java/cuentas/Main.java
+++ src/main/java/cuentas/Main.java
@@ -1,33 +1,49 @@
 package cuentas;
-
+/***
+ * La clase main se encarga de simular operaciones de una cuenta bancaria
+ * inicia una cuenta bancaria y realiza varias operaciones como ver saldo disponible y retiradas y depositos de dinero
+ * @author mayan
+ */
 
 
 public class Main {
-
+/**
+ * El metodo main sirve para iniciar la entrada al programa
+ * en el se inicia la cuenta y empieza las operaciones con esta
+ * @param args argumentos
+ */
     public static void main(String[] args) {
-        CCuenta cuenta1;
-        double saldoActual;
+        CCuenta cuenta1; //variable de la cuenta bancaria
+        double saldoActual; //variable que guarda el saldo disponible
 
-        operativa_cuenta();
+        operativa_cuenta(); //llama al metodo a hacer las operaciones oportunas
     }
-
+/***
+ * realiza las operaciones de consulta de saldo, retiradas y depositos,
+ * crea un nuevo objeto CCuenta que realiza las siguientes operaciones:
+ * retirada de cantidad especifica de saldo
+ * ingreso de una cantidad especificia
+ * consulta de disponible
+ */
     private static void operativa_cuenta() {
-        CCuenta cuenta1;
-        double saldoActual;
-        float cantidad;
-        cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
-        saldoActual = cuenta1.estado();
+        CCuenta cuenta1;//declaracion del nuevo objeto
+        double saldoActual;//variable con el saldo actual
+        float cantidad;//variable con la cantidad a retirar o ingresar
+        cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);//creacion de cuenta con nombre, numero de cuenta y saldo disponible
+        saldoActual = cuenta1.estado();//recoge el saldo actual y lo imprime por consola
         System.out.println("El saldo actual es"+ saldoActual );
+        //intenta retirar dinero y maneja las posibles excepciones
         try {
             cuenta1.retirar(2300);
         } catch (Exception e) {
-            System.out.print("Fallo al retirar");
+            System.out.print("Fallo al retirar"); //imprime por consola que la retirada no fue producida
         }
+        //intenta ingresar dinero y maneja las posibles excepciones
         try {
             System.out.println("Ingreso en cuenta");
             cuenta1.ingresar(695);
         } catch (Exception e) {
-            System.out.print("Fallo al ingresar");
+            System.out.print("Fallo al ingresar"); //imprime por consola que no se ha podido ingresar el dinero
         }
     }
 }
